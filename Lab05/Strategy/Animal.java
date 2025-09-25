6936e50e5cb9312e2c2472afbef688d2a95b038e
sanby735 <sanaybahl@gmail.com>
2025-09-25 13:59:19 -0400

    Add files via upload

---
+++ Lab05/Strategy/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public Flys flyingType;
+    public String tryToFly(){
+        return flyingType.fly();
+    }
+    public void setFlyingAbility(Flys newFlyType){
+        flyingType = newFlyType;
+    }
+}
